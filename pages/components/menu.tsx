3a63e2a0c8b478e30e13c860e00d3c47cffb6037
Alisson Santos <craftgames.alysson@gmail.com>
2021-03-16 15:44:12 -0300

    side menu complete

--- pages/components/menu.tsx
+++ pages/components/menu.tsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { ResizableBox } from 'react-resizable';
+import { ProSidebar, Menu as SideMenu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 import Avatar from './Avatar';
 
-export default function Menu() {
+interface Isubmenu {
+  id: number;
+  name: string;
+}
+
+interface Imenu {
+  id: number;
+  name: string;
+  subMenus: Isubmenu[];
+}
+
+export default function Menu(props: { content: Imenu[] }) {
   const [height, setHeight] = useState<number>(0);
 
   useEffect(() => {
@@ -14,16 +27,19 @@ export default function Menu() {
 
   return (
     <ResizableBox
-      width={200}
+      width={295}
       height={height}
       className="box"
-      minConstraints={[180, height]}
-      maxConstraints={[340, height]}
+      minConstraints={[295, height]}
+      maxConstraints={[450, height]}
       axis="x"
       handle={<span className="custom-handle custom-handle-se" />}
     >
-      <div className="scrollbar border-r-2 border-gray-300 fixed z-30 inset-y-0 left-0 h-full transition duration-300 transform bg-white dark:bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 -translate-x-full ease-in">
-        <div className="flex items-center justify-around mt-6">
+      <div
+        className="scrollbar border-r-2 border-gray-300 fixed z-30 inset-y-0 left-0 h-full transition duration-300 transform overflow-y-hidden lg:translate-x-0 lg:static lg:inset-0 -translate-x-full ease-in"
+        style={{ backgroundColor: '#1d1d1d' }}
+      >
+        <div className="flex items-center justify-around my-6">
           <Avatar color="white" />
           <div className="flex items-center justify-center">
             <button className="flex bg-blue-500 shadow-sm p-1 px-8 -ml-2 rounded-md text-white dark:text-white text-lg">
@@ -34,112 +50,18 @@ export default function Menu() {
             </button>
           </div>
         </div>
-
-        <nav className="flex flex-col mt-8 px-4 text-center">
-          <a
-            href="#"
-            className=" border-blue-400 relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-2 border-transparent pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">{/* 2 */}</span>
-            <span className="ml-2 text-md tracking-wide text-blue-500">Inbox</span>
-            <span className="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-orange bg-blue-50 rounded-full">
-              12
-            </span>
-          </a>
-
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">{/* 3 */}</span>
-            <span className="ml-2 text-md tracking-wide text-gray-600 text-base">Starred</span>
-          </a>
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">{/*  */}</span>
-            <span className="ml-2 text-md tracking-wide text-gray-600 text-base">Snoozed</span>
-          </a>
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">{/*  */}</span>
-            <span className="ml-2 text-md tracking-wide text-gray-600 text-base">Sent</span>
-          </a>
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">{/*  */}</span>
-            <span className="ml-2 text-md tracking-wide text-gray-600 text-base">Draft</span>
-          </a>
-
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">
-              {/* <svg
-              width="1.8em"
-              height="1.8em"
-              viewBox="0 0 16 16"
-              className="bi bi-exclamation border-2 border-gray-400 rounded-full text-gray-400"
-              fill="currentColor"
-            >
-              <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"></path>
-            </svg> */}
-            </span>
-            <span className="ml-2 text-md tracking-wide text-gray-600  text-base">Spam</span>
-          </a>
-
-          <a
-            href="#"
-            className=" relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800   pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">
-              {/* <svg
-              width="1.8em"
-              height="1.8em"
-              viewBox="0 0 16 16"
-              className="bi bi-exclamation border-2 border-gray-400 rounded-full text-gray-400"
-              fill="currentColor"
-            >
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
-            </svg> */}
-            </span>
-            <span className="ml-2 text-md tracking-wide text-gray-600  text-base">Bin</span>
-          </a>
-
-          <hr className="mt-2" />
-
-          <a
-            href="#"
-            className="  relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800  border-transparent pr-6"
-          >
-            <span className="inline-flex justify-center items-center ml-4">
-              {/* <svg
-              className="h-6 w-6 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-              ></path>
-            </svg> */}
-            </span>
-            <span className="ml-2 text-sm tracking-wide truncate">Labels</span>
-            <span className="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-gray-400 bg-gray-200 rounded-full">
-              4
-            </span>
-          </a>
-        </nav>
+        {/* #1d1d1d */}
+        <ProSidebar width={'100%'} className="h-full">
+          <SideMenu>
+            {props.content.map((item: Imenu) => (
+              <SubMenu title={item.name}>
+                {item.subMenus.map((subItem: Isubmenu) => (
+                  <MenuItem>{subItem.name}</MenuItem>
+                ))}
+              </SubMenu>
+            ))}
+          </SideMenu>
+        </ProSidebar>
       </div>
     </ResizableBox>
   );
--- pages/inbox.tsx
+++ pages/inbox.tsx
@@ -1,13 +1,37 @@
-import Menu from "./components/menu";
-import List from "./components/lists";
+import Menu from './components/menu';
+import List from './components/lists';
+import { InferGetServerSidePropsType } from 'next';
 
-function Inbox(){
-    return(
-        <div className="flex flex-row w-screen h-screen">
-            <Menu/>
-            <List/>
-        </div>
-    )
+import axios from 'axios';
+
+interface Isubmenu {
+  id: number;
+  name: string;
 }
+
+interface Imenu {
+  id: number;
+  name: string;
+  subMenus: Isubmenu[];
+}
+
+function Inbox({ menus }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="flex flex-row w-screen h-screen">
+      <Menu content={menus} />
+      <List />
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(
+    'http://my-json-server.typicode.com/workinideas/vagafrontendteste/menus'
+  );
+  const menus: Imenu[] = response.data;
+  return {
+    props: { menus }, // will be passed to the page component as props
+  };
+};
 
 export default Inbox;
